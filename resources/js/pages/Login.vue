2e778340c23135df3726b8a76f7bf3955f9e2db8
Gab <GabLB@outlook.com>
2026-02-19 21:59:14 -0500

    wip

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -1,4 +1,16 @@
 <template>
-    <div></div>
+    <form action="/login" class="grid h-50 place-content-center">
+        <label for="email">Email</label>
+        <input
+            autofocus
+            class=""
+            placeholder="Email"
+            id="email"
+            type="email"
+            required
+        />
+        <input id="password" type="password" required />
+        <button>LOGIN!</button>
+    </form>
 </template>
 <script setup lang="ts"></script>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Laravel\Fortify\Features;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
-})->name('home');
+    return Inertia::render('UserHome');
+})->middleware(['auth'])->name('home');
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/log', [AuthController::class, 'login'])->name('login2');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/userRoutes.php';
